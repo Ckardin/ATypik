a6bb393706480e82a50614cced9b5e84367b42b8
F&nµx <fenyx.ckardin@gmail.com>
2024-11-02 21:17:35 +0100

    Add header of Utils.

---
+++ Utils.h
@@ -0,0 +1,40 @@
+/*
+---------------------------------------------
+|    ____                                   |
+|   /\  _`\                                 |
+|   \ \ \L\_\ __    ___   __  __   __  _    |
+|    \ \  _\/'__`\/' _ `\/\ \/\ \ /\ \/'\   |
+|     \ \ \/\  __//\ \/\ \ \ \_\ \\/>  </   |
+|      \ \_\ \____\ \_\ \_\/`____ \/\_/\_\  |
+|       \/_/\/____/\/_/\/_/`/___/> \//\/_/  |
+|                             /\___/        |
+|                             \/__/         |
+|                                           |
+---------------------------------------------
+*/
+
+/// @file Utils.h
+/// @brief Header de Utils
+/// @author F&nµx
+/// @version 1.0
+/// @date 02/11/2024
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "Defines.h"
+
+namespace Fenyx::Types
+{
+
+QWORD GetTimestamp();
+QWORD GetTimestampMilli();
+
+char toChar(BYTE a);
+BYTE toAscii(char c);
+
+DWORD MicroChaoCrypt(DWORD orand);
+
+}
+
+#endif //UTILS_H
