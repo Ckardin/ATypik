41c9ddbab4cfe5b2c9faff8de6fe47e5847aa862
F&nµx <fenyx.ckardin@gmail.com>
2024-11-02 20:24:34 +0100

    Add source of Tabs.

--- Tabs.cpp
+++ Tabs.cpp
@@ -24,7 +24,37 @@
 namespace Fenyx::Types
 {
 
+#ifdef LITTLE_ENDIAN
+	void SwapBytes(barray &bytes) {
+		BYTE temp, j;
 
+		for(BYTE i = 1; i < (bytes[0] / 2); i = i + 1) {
+			j = (bytes[0] - 1) - i;
+
+			temp     = bytes[i];
+			bytes[i] = bytes[j];
+			bytes[j] = temp;
+		}
+	}
+#endif // LITTLE_ENDIAN
+
+barray BytesArray(allnum data) {
+	using T = std::decay_t<decltype(data)>;
+	barray bytes;
+
+	if (!bytes.GetFatal()) {
+		bytes[0] = sizeof(T);
+		std::visit([&bytes](auto && v) -> void {
+			for(BYTE i = 1; i < static_cast<BYTE>(sizeof(T)); i = i + 1) bytes[i] = v >> (i * 8);
+		}, data);
+
+#ifdef LITTLE_ENDIAN
+		SwapBytes(bytes);
+#endif // LITTLE_ENDIAN
+	}
+
+	return bytes;
+}
 
 }
 
